ba30839bf8592b9dae39dc5b937553ac2484f9bd
sunnie1234 <sunnie.pallante@elev.ga.ntig.se>
2023-08-31 14:36:37 +0200

    uybhuyhbyub

---
+++ BashMonster/Fish.cs
@@ -0,0 +1,8 @@
+namespace Namespace;
+public class Fish
+  {
+  public bool IsEdible = true;
+  public int Damage = 100;
+  public float Weight = 20;
+  public bool Stackable = true;
+  }
---
+++ BashMonster/Program.cs
@@ -0,0 +1,15 @@
+﻿using Namespace;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Fish wanda = new Fish();
+Fish henry = new Fish();
+
+wanda.Damage = 100;
+henry.Damage = 200;
+
+Console.WriteLine(wanda.Damage);
+
+Console.ReadLine();
