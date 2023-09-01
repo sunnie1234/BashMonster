aebcd2626aa13172b5eb875a3d2009292edd90ea
sunnie1234 <sunnie.pallante@elev.ga.ntig.se>
2023-09-01 12:42:17 +0200

    la till en shield

--- BashMonster/Fish.cs
+++ BashMonster/Fish.cs
@@ -5,4 +5,18 @@ public class Fish
   public int Damage = 100;
   public float Weight = 20;
   public bool Stackable = true;
+
+  public int maxHp = 100;
+  public int hp;
+
+  public Fish()
+  {
+    maxHp = new Random().Next(90,110);
+     ResetHp();
+  }
+
+   public void ResetHp()
+  {
+    hp = maxHp;
+  } 
   }
--- BashMonster/Program.cs
+++ BashMonster/Program.cs
@@ -5,11 +5,12 @@ Console.WriteLine("Hello, World!");
 
 
 Fish wanda = new Fish();
+
 Fish henry = new Fish();
+
 
 wanda.Damage = 100;
 henry.Damage = 200;
 
-Console.WriteLine(wanda.Damage);
 
 Console.ReadLine();
